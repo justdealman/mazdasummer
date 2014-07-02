7c6a003f4286ce6d382aedfa5490675d855ac960
Alexandr <justdealman@gmail.com>
2014-07-02 12:15:27 +0700

    original

---
+++ js/dynamic.js
@@ -0,0 +1,13 @@
+﻿$(document).ready(function() {
+	$('input[type="checkbox"]').uniform();
+	$('.form .nickname').hide();
+	$('.form input[type="checkbox"]').change(function() {
+		if ( $('.form input[type="checkbox"]:checked').length > 0 ) {
+			$('.form .nickname').show();
+		}
+		else {
+			$('.form .nickname').hide();
+		}
+		return false;
+	});
+});
